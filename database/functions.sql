c3d4089d0c4f79abdca230f9bb2356603dde52be
Jepp0103 <jeppendyekjaer@hotmail.com>
2021-03-10 12:42:02 +0100

    Added functions and views

--- database/functions.sql
+++ database/functions.sql
@@ -1,2 +1,17 @@
-#A function that takes a student and a course as arguments and gets the attendance rate for that student and that course. 
-#If course is null then get the attendance rate for the student across all lectures regardless of course.
+#1. A function that takes a student and a course as arguments and gets the attendance rate 
+#for that student and that course. If course is null then get the attendance rate for the 
+#student across all lectures regardless of course.
+DELIMITER $$
+CREATE FUNCTION getStudentAttendanceRate(
+	forename VARCHAR(20),
+    surname VARCHAR(20),
+    course_name VARCHAR(20)
+)
+RETURNS INT(11)
+DETERMINISTIC
+BEGIN
+	DECLARE StudentAttendanceRate INT(11);
+	SET StudentAttendanceRate = #what to do from here?
+
+
+#COUNT(registred_at);
---
+++ database/views.sql
@@ -0,0 +1,3 @@
+#insertions for gps_coordinates, teacher and related students
+
+#INSERT INTO gps_coordinates VALUES (1,
