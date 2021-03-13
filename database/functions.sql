4ed51f9b7a37692dd88dc6d18c9e731c7a5db0a3
Jepp0103 <jeppendyekjaer@hotmail.com>
2021-03-13 17:45:34 +0100

    Modified attendance rate function - currently it returns the rate as an integer instead of a decimal as wanted.

--- database/functions.sql
+++ database/functions.sql
@@ -15,9 +15,22 @@ INSERT INTO `attendance_record` (`student_id`, `lecture_id`, `is_attending`, `re
 INSERT INTO `attendance_record` (`student_id`, `lecture_id`, `is_attending`, `registred_at`) VALUES ('Jeppe@stud.kea.dk', 3, 0, '2021-02-15 08:25:00');
 INSERT INTO `attendance_record` (`student_id`, `lecture_id`, `is_attending`, `registred_at`) VALUES ('Jeppe@stud.kea.dk', 4, 0, '2021-01-15 08:25:00');
 INSERT INTO `attendance_record` (`student_id`, `lecture_id`, `is_attending`, `registred_at`) VALUES ('Jeppe@stud.kea.dk', 8, 1, '2021-07-15 08:25:00');
+INSERT INTO `attendance_record` (`student_id`, `lecture_id`, `is_attending`, `registred_at`) VALUES ('Jeppe@stud.kea.dk', 9, 0, '2021-07-15 08:25:00');
+INSERT INTO `attendance_record` (`student_id`, `lecture_id`, `is_attending`, `registred_at`) VALUES ('Jeppe@stud.kea.dk', 10, 1, '2021-03-15 08:25:00');
+
+INSERT INTO `attendance_record` (`student_id`, `lecture_id`, `is_attending`, `registred_at`) VALUES ('Immanuel@stud.kea.dk', 2, 0, '2021-04-15 08:25:00');
+INSERT INTO `attendance_record` (`student_id`, `lecture_id`, `is_attending`, `registred_at`) VALUES ('Immanuel@stud.kea.dk', 3, 0, '2021-02-15 08:25:00');
+INSERT INTO `attendance_record` (`student_id`, `lecture_id`, `is_attending`, `registred_at`) VALUES ('Immanuel@stud.kea.dk', 4, 0, '2021-01-15 08:25:00');
+INSERT INTO `attendance_record` (`student_id`, `lecture_id`, `is_attending`, `registred_at`) VALUES ('Immanuel@stud.kea.dk', 8, 1, '2021-07-15 08:25:00');
+INSERT INTO `attendance_record` (`student_id`, `lecture_id`, `is_attending`, `registred_at`) VALUES ('Immanuel@stud.kea.dk', 9, 0, '2021-07-15 08:25:00');
+INSERT INTO `attendance_record` (`student_id`, `lecture_id`, `is_attending`, `registred_at`) VALUES ('Immanuel@stud.kea.dk', 10, 1, '2021-03-15 08:25:00');
+
 
 SELECT * FROM attendance_record
 	WHERE student_id = 'Jeppe@stud.kea.dk';
+
+SELECT * FROM attendance_record
+	WHERE student_id = 'Immanuel@stud.kea.dk';
 
 SELECT * FROM lecture 
 	WHERE course_id = 1;
@@ -34,27 +47,26 @@ DETERMINISTIC
 BEGIN
 	DECLARE studentLectureAttendanceRate DECIMAL;
 	DECLARE amountOfAttendances INT;
+	DECLARE amountOfLecturesForCourse INT;
+
     
     SET amountOfAttendances = (SELECT count(is_attending) FROM attendance_record ar
 								JOIN lecture l ON ar.lecture_id = l.id
 								JOIN course c ON l.course_id = c.id
 								WHERE is_attending = 1 AND student_id = arg_student_id AND c.id = arg_course_id);
     
-	SET studentLectureAttendanceRate = (SELECT count(*) FROM lecture l
+	SET amountOfLecturesForCourse = (SELECT count(*) FROM lecture l
 										JOIN course c ON l.course_id = c.id
-										WHERE c.id = arg_course_id)/amountOfAttendances;
-	RETURN (studentLectureAttendanceRate);
+										WHERE c.id = arg_course_id);
+    
+	SET studentLectureAttendanceRate = amountOfAttendances/amountOfLecturesForCourse;
+                                        
+	RETURN (CAST(studentLectureAttendanceRate AS DECIMAL));
 END$$
 
+
+#TESTING THE FUNCTION
 SELECT getStudentLectureAttendanceRate('Jeppe@stud.kea.dk', 1);  
-    
-SELECT count(is_attending) FROM attendance_record ar
-								JOIN lecture l ON ar.lecture_id = l.id
-								JOIN course c ON l.course_id = c.id
-								WHERE is_attending = 1 AND student_id = 'Jeppe@stud.kea.dk' AND c.id = 1;    
-    
-SELECT count(*) FROM lecture l
-										JOIN course c ON l.course_id = c.id
-										WHERE c.id = arg_course_id)/amountOfAttendances;
+SELECT getStudentLectureAttendanceRate('Immanuel@stud.kea.dk', 1); 
     
 #-------------------------------------------------------------------------------
