cd3ee6fcdaf3dcdb71af142307b62e99f01e92ff
Jepp0103 <jeppendyekjaer@hotmail.com>
2021-03-13 15:17:38 +0100

    Solving revert conflict due to insertion errors I want to revert away from

--- database/functions.sql
+++ database/functions.sql
@@ -10,16 +10,8 @@ CREATE FUNCTION getStudentAttendanceRate(
 RETURNS INT(11)
 DETERMINISTIC
 BEGIN
-	DECLARE studentLectureAttendanceRate DECIMAL;
-	DECLARE amountOfAttendances INT;
-    
-    SET amountOfAttendances = (SELECT count(is_attending) FROM attendance_record ar
-								JOIN lecture l ON ar.lecture_id = l.id
-								JOIN course c ON l.course_id = c.id
-								WHERE is_attending = 1 AND student_id = arg_student_id AND c.id = arg_course_id);
-    
-	SET studentLectureAttendanceRate = (SELECT count(*) FROM lecture l
-										JOIN course c ON l.course_id = c.id
-										WHERE c.id = arg_course_id)/amountOfAttendances;
-	RETURN (studentLectureAttendanceRate);
-END$$
+	DECLARE StudentAttendanceRate INT(11);
+	SET StudentAttendanceRate = #what to do from here?
+
+
+#COUNT(registred_at);
