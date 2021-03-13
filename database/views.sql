3261f0a1e7ba844cd4a561de06e324054cc86653
Jepp0103 <jeppendyekjaer@hotmail.com>
2021-03-13 15:21:11 +0100

    Solving revert conflict due to insertion errors I want to revert away from

--- database/views.sql
+++ database/views.sql
@@ -2,15 +2,11 @@ CREATE OR REPLACE VIEW `get_gps_coordinates_student` AS
 SELECT latitude, longitude, concat(s.forename, " ", s.surname) as student
 FROM gps_coordinates g
     JOIN student s ON g.id = s.gps_id;
-SELECT *
-FROM `get_gps_coordinates_student
-`;
+SELECT * FROM `get_gps_coordinates_student`;
 
 CREATE OR REPLACE VIEW `get_gps_coordinates_teacher` AS
 SELECT latitude, longitude, concat(t.forename, " ", t.surname) as teacher
 FROM gps_coordinates g
     JOIN teacher t ON g.id = t.gps_coordinates_id;
-
-SELECT *
-FROM `get_gps_coordinates_teacher
-`;
+    
+SELECT * FROM `get_gps_coordinates_teacher`;
