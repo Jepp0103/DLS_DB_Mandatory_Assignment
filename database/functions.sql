1ebf9702d0d30275a4cff9de82c61618eefb72e3
Immanuellllllll <44088357+Immanuellllllll@users.noreply.github.com>
2021-03-15 10:47:15 +0100

    Additions to the attendence rate function

--- database/functions.sql
+++ database/functions.sql
@@ -1,39 +1,3 @@
-#1. A function that takes a student and a course as arguments and gets the attendance rate 
-#for that student and that course. If course is null then get the attendance rate for the 
-#student across all lectures regardless of course.
-
-#Test data for the function
-#Adding extra lectures to attendance rate for function
-INSERT INTO `lecture` (`course_id`, `classroom_id`, `name`, `date`, `time_start`, `time_end`, `time_zone`, `length`) VALUES ('1', '1', 'DB lecture', '2021-03-15 08:15:00', '08:15:00', '13:30:00', '0', '0');
-INSERT INTO `lecture` (`course_id`, `classroom_id`, `name`, `date`, `time_start`, `time_end`, `time_zone`, `length`) VALUES ('1', '2', 'DB lecture 2', '2021-03-17 08:15:00', '08:15:00', '13:30:00', '0', '0');
-INSERT INTO `lecture` (`course_id`, `classroom_id`, `name`, `date`, `time_start`, `time_end`, `time_zone`, `length`) VALUES ('1', '2', 'DB lecture 3', '2021-03-18 08:15:00', '08:15:00', '13:30:00', '0', '0');
-INSERT INTO `lecture` (`course_id`, `classroom_id`, `name`, `date`, `time_start`, `time_end`, `time_zone`, `length`) VALUES ('1', '2', 'DB lecture 4', '2021-03-19 08:15:00', '08:15:00', '13:30:00', '0', '0');
-INSERT INTO `lecture` (`course_id`, `classroom_id`, `name`, `date`, `time_start`, `time_end`, `time_zone`, `length`) VALUES ('1', '2', 'DB lecture 5', '2021-03-20 08:15:00', '08:15:00', '13:30:00', '0', '0');
-
-#Extra attendances for Jeppe to test function
-INSERT INTO `attendance_record` (`student_id`, `lecture_id`, `is_attending`, `registred_at`) VALUES ('Jeppe@stud.kea.dk', 2, 1, '2021-04-15 08:25:00');
-INSERT INTO `attendance_record` (`student_id`, `lecture_id`, `is_attending`, `registred_at`) VALUES ('Jeppe@stud.kea.dk', 3, 0, '2021-02-15 08:25:00');
-INSERT INTO `attendance_record` (`student_id`, `lecture_id`, `is_attending`, `registred_at`) VALUES ('Jeppe@stud.kea.dk', 4, 0, '2021-01-15 08:25:00');
-INSERT INTO `attendance_record` (`student_id`, `lecture_id`, `is_attending`, `registred_at`) VALUES ('Jeppe@stud.kea.dk', 8, 1, '2021-07-15 08:25:00');
-INSERT INTO `attendance_record` (`student_id`, `lecture_id`, `is_attending`, `registred_at`) VALUES ('Jeppe@stud.kea.dk', 9, 0, '2021-07-15 08:25:00');
-INSERT INTO `attendance_record` (`student_id`, `lecture_id`, `is_attending`, `registred_at`) VALUES ('Jeppe@stud.kea.dk', 10, 1, '2021-03-15 08:25:00');
-
-INSERT INTO `attendance_record` (`student_id`, `lecture_id`, `is_attending`, `registred_at`) VALUES ('Immanuel@stud.kea.dk', 2, 0, '2021-04-15 08:25:00');
-INSERT INTO `attendance_record` (`student_id`, `lecture_id`, `is_attending`, `registred_at`) VALUES ('Immanuel@stud.kea.dk', 3, 0, '2021-02-15 08:25:00');
-INSERT INTO `attendance_record` (`student_id`, `lecture_id`, `is_attending`, `registred_at`) VALUES ('Immanuel@stud.kea.dk', 4, 0, '2021-01-15 08:25:00');
-INSERT INTO `attendance_record` (`student_id`, `lecture_id`, `is_attending`, `registred_at`) VALUES ('Immanuel@stud.kea.dk', 8, 1, '2021-07-15 08:25:00');
-INSERT INTO `attendance_record` (`student_id`, `lecture_id`, `is_attending`, `registred_at`) VALUES ('Immanuel@stud.kea.dk', 9, 0, '2021-07-15 08:25:00');
-INSERT INTO `attendance_record` (`student_id`, `lecture_id`, `is_attending`, `registred_at`) VALUES ('Immanuel@stud.kea.dk', 10, 1, '2021-03-15 08:25:00');
-
-
-SELECT * FROM attendance_record
-	WHERE student_id = 'Jeppe@stud.kea.dk';
-
-SELECT * FROM attendance_record
-	WHERE student_id = 'Immanuel@stud.kea.dk';
-
-SELECT * FROM lecture 
-	WHERE course_id = 1;
 
 #THE FUNCTION
 DROP FUNCTION IF EXISTS getStudentLectureAttendanceRate;
@@ -48,25 +12,22 @@ BEGIN
 	DECLARE studentLectureAttendanceRate DECIMAL;
 	DECLARE amountOfAttendances INT;
 	DECLARE amountOfLecturesForCourse INT;
+	DECLARE class VARCHAR(100);
+    
+    set @classname =(select class.name from student join class on student.class_faculty_id=class.faculty_id and student.class_name=class.name where student.email_address=arg_student_id);
 
+    
     
     SET amountOfAttendances = (SELECT count(is_attending) FROM attendance_record ar
 								JOIN lecture l ON ar.lecture_id = l.id
 								JOIN course c ON l.course_id = c.id
-								WHERE is_attending = 1 AND student_id = arg_student_id AND c.id = arg_course_id);
+								WHERE is_attending = 1 AND student_id = arg_student_id AND c.id like if(arg_course_id>0,arg_course_id,"%"));
     
 	SET amountOfLecturesForCourse = (SELECT count(*) FROM lecture l
-										JOIN course c ON l.course_id = c.id
-										WHERE c.id = arg_course_id);
+										JOIN course c ON l.course_id = c.id join class_lecture as cl on l.id = cl.lecture_id
+										WHERE c.id like if(arg_course_id>0,arg_course_id,"%") and cl.class_id=@classname);
     
-	SET studentLectureAttendanceRate = amountOfAttendances/amountOfLecturesForCourse;
+	SET studentLectureAttendanceRate = amountOfAttendances/amountOfLecturesForCourse*100;
                                         
 	RETURN (CAST(studentLectureAttendanceRate AS DECIMAL));
 END$$
-
-
-#TESTING THE FUNCTION
-SELECT getStudentLectureAttendanceRate('Jeppe@stud.kea.dk', 1);  
-SELECT getStudentLectureAttendanceRate('Immanuel@stud.kea.dk', 1); 
-    
-#-------------------------------------------------------------------------------
