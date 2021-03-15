f755dd20ad2197b3dee2d871daa1b79e3f604f4b
Jepp0103 <jeppendyekjaer@hotmail.com>
2021-03-15 11:55:56 +0100

    Beautified functions.sql and changed return type into an int from decimal

--- database/functions.sql
+++ database/functions.sql
@@ -1,4 +1,3 @@
-
 #THE FUNCTION
 DROP FUNCTION IF EXISTS getStudentLectureAttendanceRate;
 DELIMITER $$
@@ -6,28 +5,27 @@ CREATE FUNCTION getStudentLectureAttendanceRate(
 	arg_student_id VARCHAR(100),
     arg_course_id INT
 )
-RETURNS DECIMAL
+RETURNS INT
 DETERMINISTIC
 BEGIN
-	DECLARE studentLectureAttendanceRate DECIMAL;
+	DECLARE studentLectureAttendanceRate INT;
 	DECLARE amountOfAttendances INT;
 	DECLARE amountOfLecturesForCourse INT;
 	DECLARE class VARCHAR(100);
     
-    set @classname =(select class.name from student join class on student.class_faculty_id=class.faculty_id and student.class_name=class.name where student.email_address=arg_student_id);
+    SET @classname =(SELECT student.class_name FROM student WHERE student.email_address=arg_student_id);
 
-    
     
     SET amountOfAttendances = (SELECT count(is_attending) FROM attendance_record ar
 								JOIN lecture l ON ar.lecture_id = l.id
 								JOIN course c ON l.course_id = c.id
-								WHERE is_attending = 1 AND student_id = arg_student_id AND c.id like if(arg_course_id>0,arg_course_id,"%"));
+								WHERE is_attending = 1 AND student_id = arg_student_id AND c.id LIKE IF(arg_course_id>0,arg_course_id,"%"));
     
 	SET amountOfLecturesForCourse = (SELECT count(*) FROM lecture l
-										JOIN course c ON l.course_id = c.id join class_lecture as cl on l.id = cl.lecture_id
-										WHERE c.id like if(arg_course_id>0,arg_course_id,"%") and cl.class_id=@classname);
+										JOIN course c ON l.course_id = c.id JOIN class_lecture AS cl ON l.id = cl.lecture_id
+										WHERE c.id LIKE IF(arg_course_id>0,arg_course_id,"%") AND cl.class_id=@classname);
     
 	SET studentLectureAttendanceRate = amountOfAttendances/amountOfLecturesForCourse*100;
                                         
-	RETURN (CAST(studentLectureAttendanceRate AS DECIMAL));
+	RETURN (studentLectureAttendanceRate);
 END$$
