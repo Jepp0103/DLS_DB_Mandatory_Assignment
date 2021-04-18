5187e11a1b8f380c04f9b9dc7eac7be69dd59980
Jepp0103 <jeppendyekjaer@hotmail.com>
2021-04-18 21:17:41 +0200

    Working on registerStudentGps stored procedure

--- database/stored_procedures.sql
+++ database/stored_procedures.sql
@@ -14,3 +14,27 @@ BEGIN
     WHERE class_id = class_id_arg;
 END $$
 DELIMITER ;
+
+#A stored procedure, which will insert network and GPS records for a student (or only GPS for a teacher), 
+#once they are successfully retrieved from the front-end, 
+#a request will be sent to the back-end and the back-end 
+#will call this stored procedure providing the correct parameters. 
+#What could be returned is if the student is within the range of the 
+#teacher’s registered GPS coordinates.
+DROP PROCEDURE IF EXISTS registerStudentGps;
+DELIMITER $$
+CREATE PROCEDURE registerStudentGps(
+	IN student_id INT,
+    IN gps_latitude DECIMAL(10,8),
+    IN gps_longitude DECIMAL(11,8),
+    IN gps_range DECIMAL(6,5),
+    OUT within_range CHAR(1)
+)
+BEGIN 
+	INSERT INTO `gps_coordinates` (`latitude`, `longitude`, `range`) VALUES (gps_latitude, gps_longitude, gps_range);
+    
+END $$
+DELIMITER ;
+    
+    
+#INSERT INTO `gps_coordinates` (`latitude`, `longitude`, `range`) VALUES ('60.70392118', '22.537521047',7.55555);
