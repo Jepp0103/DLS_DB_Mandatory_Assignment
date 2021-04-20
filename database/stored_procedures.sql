d10b703ddb1dd6a5f56f5b444d7c1bce57a1ddfa
Jepp0103 <jeppendyekjaer@hotmail.com>
2021-04-20 18:42:43 +0200

    Main functionality of registerStudentGps stored procedure seems to work in all those test cases I have tried out when validating gps distance from one student to one teacher

--- database/stored_procedures.sql
+++ database/stored_procedures.sql
@@ -21,51 +21,68 @@ DELIMITER ;
 #will call this stored procedure providing the correct parameters. 
 #What could be returned is if the student is within the range of the 
 #teacher’s registered GPS coordinates.
-DROP PROCEDURE IF EXISTS registerStudentGps;
+DROP PROCEDURE IF EXISTS register_student_gps;
 DELIMITER $$
-CREATE PROCEDURE registerStudentGps(
+CREATE PROCEDURE register_student_gps(
 	IN student_id INT,
-    IN gps_latitude DECIMAL(10,8),
-    IN gps_longitude DECIMAL(11,8),
+    IN teacher_id INT,
+    IN student_latitude DECIMAL(10,8),
+    IN student_longitude DECIMAL(11,8),
     IN gps_range DECIMAL(6,5),
-    OUT within_range CHAR(1)
+	OUT within_range CHAR(50)
 )
 BEGIN 
-	#Validate before insertion that all parameter values are valid.
-	DECLARE addedGpsId INT;
-
-	INSERT INTO `gps_coordinates` (`latitude`, `longitude`, `range`) VALUES (gps_latitude, gps_longitude, gps_range);
-
-	#SET addedGpsId = (SELECT LAST_INSERT_ID());
-    SELECT LAST_INSERT_ID() INTO addedGpsId;
-#	SET @addedGpsId = (SELECT id FROM gps_coordinates
-#						WHERE latitude = gps_latitude AND longitude = gps_longitude AND `range` = gps_range);
-                  
-	#Updating coordinates for the student
-	UPDATE student
-	SET gps_coordinates_id = addedGpsId
-	WHERE id = student_id;
-	
-    set within_range = 'n';
+	DECLARE added_gps_id INT;
+    DECLARE teacher_latitude DECIMAL(10,8);
+    DECLARE teacher_longitude DECIMAL(11,8);
+    DECLARE lat_deg_dist DECIMAL (10,3);
+    DECLARE long_deg_dist DECIMAL (8,3);
     
-    #select 'n' into within_range;
-     
-       #OUT within_range CHAR(1)1
-    #OUT addedGpsId INT
+
+			INSERT INTO `gps_coordinates` (`latitude`, `longitude`, `range`) VALUES (student_latitude, student_longitude, gps_range);
+			SELECT LAST_INSERT_ID() INTO added_gps_id;
+    
+			#Updating coordinates for the student
+			UPDATE student
+			SET gps_coordinates_id = added_gps_id
+			WHERE id = student_id;
+
+			#Getting latitude and longitude values for a teacher.
+			SET teacher_latitude = (SELECT latitude FROM gps_coordinates gc
+										JOIN teacher t ON gc.id = t.gps_coordinates_id
+										WHERE t.id = teacher_id);
+								   
+			SET teacher_longitude = (SELECT longitude FROM gps_coordinates gc
+										JOIN teacher t ON gc.id = t.gps_coordinates_id
+										WHERE t.id = teacher_id);
+		   
+			#Source for calculating distance - https://www.usgs.gov/faqs/how-much-distance-does-a-degree-minute-and-second-cover-your-maps?qt-news_science_products=0#qt-news_science_products
+			SET lat_deg_dist = 111033.736; #Distance for one latitude degree in meters
+			SET long_deg_dist = 87870.182; #Distance for one longitude degree in meters
+			
+			#Comparing gps values for teacher and student meter distance and returning within range value. 
+			IF  (teacher_latitude = student_latitude AND teacher_longitude = student_longitude) #When student gps values are equal to teacher gps values
+			THEN
+				SELECT 'e' INTO within_range;
+				
+			ELSEIF (teacher_latitude > student_latitude OR teacher_longitude > student_longitude) #When gps values are higher for a teacher than a student but still within 9.99999 meters range
+				AND ((teacher_latitude * lat_deg_dist) - (student_latitude * lat_deg_dist)) <= 9.99999 
+				AND ((teacher_longitude * long_deg_dist) - (student_longitude * long_deg_dist)) <= 9.99999
+			THEN
+				SELECT 't' INTO within_range;
+				
+			ELSEIF (student_latitude > teacher_latitude OR student_longitude > teacher_longitude) #When gps values are higher for a student than a teacher but still within 9.99999 meters range 
+			AND ((student_latitude * lat_deg_dist) - (teacher_latitude * lat_deg_dist)) <= 9.99999 
+			AND ((student_longitude * long_deg_dist) - (teacher_longitude * long_deg_dist)) <= 9.99999 
+			THEN
+				SELECT 's' INTO within_range;
+				
+			ELSE 
+				SELECT 'n' INTO within_range; 
+			END IF;
+
+	
 END $$
 DELIMITER ;
 
-set @hi = '';
-CALL registerStudentGps(1, 70.75492148, 50.53352305, 4.53435, @hi);
-select @hi;
 
-
-select * from gps_coordinates;
-
-select * from student s
-	inner join gps_coordinates gc on s.gps_coordinates_id = gc.id; 
-
-select * from lecture;
-
-select * from classroom;
-select * from course;
