51d29fb92b1023bdecdee1ddb5faa9ff84372e49
Jepp0103 <jeppendyekjaer@hotmail.com>
2021-04-29 19:44:36 +0200

    Added react front end folder and renamed application folder to roll call backend

--- application/frontend_roll_call_system/roll-call-app/src/home.js
+++ application/frontend_roll_call_system/roll-call-app/src/home.js
@@ -1,19 +1,6 @@
 import React, { Component } from "react";
 import styled from 'styled-components';
 
-const OverAllView = styled.View`
-    background-color: green;
-    width: 100%;
-    height: 100%;
-`;
-
-const CurrentClassesBox = styled.Text`
-    font-size: 12px;
-    align-self: flex-end;
-    color: white;
-    margin-right: 5%;
-    font-weight: bold;
-`;
 
 class home extends Component {
   handleLogout() {
@@ -23,22 +10,18 @@ class home extends Component {
 
   render() {
     return (
-      <OverAllView>
 
-        <CurrentClassesBox>Hello</CurrentClassesBox>
-      </OverAllView>
-
-      //   <div>
-      //     <h1>Hi there</h1>
-
-      //     <a
-      //       href="javascript:void(0);"
-      //       onClick={this.handleLogout}
-      //       className="d-b td-n pY-5 bgcH-grey-100 c-grey-700">
-      //       <i className="ti-power-off mR-10"></i>
-      //       <span style={{ color: "white" }}>Logout</span>
-      //     </a>
-      //   </div>
+        <div>
+          <h1>Hi there</h1>
+          
+          <a
+            href="javascript:void(0);"
+            onClick={this.handleLogout}
+            className="d-b td-n pY-5 bgcH-grey-100 c-grey-700">
+            <i className="ti-power-off mR-10"></i>
+            <span style={{ color: "white" }}>Logout</span>
+          </a>
+        </div>
     );
   }
 }
