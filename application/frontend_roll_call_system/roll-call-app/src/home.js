1b335ef3c5fde6969020241c9c8600ca5fcf8530
Jepp0103 <jeppendyekjaer@hotmail.com>
2021-04-29 21:30:44 +0200

    Removed old folder roll call system for backend

--- application/frontend_roll_call_system/roll-call-app/src/home.js
+++ application/frontend_roll_call_system/roll-call-app/src/home.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
 import styled from 'styled-components';
+import "./css/home.css";
+// const client = require('./client');
 
 
 class home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { classes: [] };
+  }
+
+  // componentDidMount() {
+  //   client({ method: 'GET', path: '/api/employees' }).done(response => {
+  //     this.setState({ employees: response.entity._embedded.employees });
+  //   });
+  // }
+
   handleLogout() {
     localStorage.clear();
     window.location.href = "/";
@@ -11,17 +24,43 @@ class home extends Component {
   render() {
     return (
 
-        <div>
-          <h1>Hi there</h1>
-          
-          <a
-            href="javascript:void(0);"
-            onClick={this.handleLogout}
-            className="d-b td-n pY-5 bgcH-grey-100 c-grey-700">
-            <i className="ti-power-off mR-10"></i>
-            <span style={{ color: "white" }}>Logout</span>
-          </a>
+      <div id="homeStudentRollCall">
+        <h1>School roll call</h1>
+        <div id="classDiv">
+          <b>Current classes:</b>
+
         </div>
+        <div id="excStudentsDiv">
+          <b>Expected students:</b>
+
+        </div>
+        <div id="attendingStudentsDiv">
+          <b>Attending students:</b>
+        </div>
+        <div id="gpsDiv">
+          <b>GPS:</b>
+
+        </div  >
+        <div id="currentCourseDiv">
+          <b>Current course:</b>
+
+        </div>
+        <div id="currentLectureDiv">
+          <b>Current lecture:</b>
+
+        </div>
+        <div id="networkDiv">
+          <b>Network:</b>
+
+        </div>
+        <a id="logout"
+          href="javascript:void(0);"
+          onClick={this.handleLogout}
+          className="d-b td-n pY-5 bgcH-grey-100 c-grey-700">
+          <i className="ti-power-off mR-10"></i>
+          <span>Logout</span>
+        </a>
+      </div>
     );
   }
 }
